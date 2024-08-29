332ba0105c91d5a0e65db1191d0cf19f4851f011
anke <245783065@qq.com>
2024-08-29 10:48:51 +0800

    feat: 修改/路由页面展示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+import AdminPage from "./(dashboard)/admin/page";
 
-export default Homepage
+export default function HomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* Left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-center gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
+          <span className="hidden lg:block">SchooLama</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* Right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        <AdminPage />
+      </div>
+    </div>
+  );
+}
