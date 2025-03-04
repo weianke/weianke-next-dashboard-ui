3c4ec5b6ff85f3657ee6fea32a1441283a5a9905
anke <245783065@qq.com>
2025-03-04 09:27:54 +0800

    update: 优化路由

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({
       {/* Left */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
         <Link
-          href="/"
+          href="/admin"
           className="flex items-center justify-center lg:justify-center gap-2"
         >
           <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
